849bfcc5a3bd1124b4c7f3b3088c139579e6952c
Amine Oudjana <amine.oudjana@withsecure.com>
2025-02-22 18:22:36 +0100

    feat: Implement Navigation Bar with Logo, Menu, About Section, and Dark/Light Mode Toggle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="menu bg-white dark:bg-gray-900">
+      <h1 class="text-black dark:text-white">This is a main page</h1>
+    </div>
   </main>
 </template>
+<script setup lang="ts"></script>
+<style>
+@media (min-width: 1024px) {
+  .menu {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import flowbite from 'flowbite/plugin'
+
+export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js"
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lobster', 'sans-serif'],
+      },
+    },
   },
   plugins: [
-    require('flowbite/plugin')
+    flowbite
   ],
 }
